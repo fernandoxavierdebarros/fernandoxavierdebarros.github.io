41744fcb2fa38bf2b2597286c77a05b5521b4e63
Fernando Barros Dell <fernandoxavierdebarros@gmail.com>
2021-04-22 18:07:21 -0300

    sdv

---
+++ callBeto/pages/home.js
@@ -0,0 +1,11 @@
+function home(){
+
+    page = "home"
+
+    var content = document.getElementById("content")
+    var titulo = document.createElement("h1")
+    titulo.innerHTML = "Hello World"
+    content.appendChild(titulo)
+    
+    
+}
